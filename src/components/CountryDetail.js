3b31432b5626f1ff8c78e0d0b20f60882bdf9b85
matheustkaczyk <matheus111904@hotmail.com>
2022-01-24 11:15:48 -0300

    Iniciando estilizaçao da parte de detalhes

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -2,31 +2,31 @@ import React from 'react';
 
 const CountryDetail = (country) => {
   return (
-    <div className="country-details" key={ country.name }>
+    <div className="country-card" key={ country.name }>
         <img src={ country.flags.svg } alt="country flag" />
-        <div>
+        <div className="country-detail-info">
           <div>
             <h2>{ country.name }</h2>
-            <p>Native name: { country.nativeName }</p>
-            <p>Population: { country.population }</p>
-            <p>Region: { country.region }</p>
-            <p>Sub region: { country.subregion }</p>
-            <p>Capital: { country.capital }</p>
+            <p>Native name: { <span>{country.nativeName}</span> }</p>
+            <p>Population: { <span>{country.population}</span> }</p>
+            <p>Region: { <span>{country.region}</span> }</p>
+            <p>Sub region: { <span>{country.subregion}</span> }</p>
+            <p>Capital: { <span>{country.capital}</span> }</p>
           </div>
           <div>
             <p>Top level domain: { country.topLevelDomain[0] }</p>
-            <p>Currencies: 
+            <span>Currencies:
               <ul>
                 {
                   country.currencies.map((currency) => <li key={currency.code}>{ currency.code }</li>)
                 }
               </ul>
-            </p>
-            <p>Languages:
+            </span>
+            <span>Languages:
               <ul>
                 { country.languages.map((language) => <li key={language.name}>{language.name}</li>) }
               </ul>
-            </p>
+            </span>
           </div>
         </div>
       </div>
