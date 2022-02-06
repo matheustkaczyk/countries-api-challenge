7cf1cf219ac8343c33446a3f4180c5ac8004c1bd
matheustkaczyk <matheus111904@hotmail.com>
2022-02-06 11:34:03 -0300

    configurado centericao da div details

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -7,14 +7,14 @@ const CountryDetail = (country) => {
         <div className="country-detail-info">
           <div>
             <h2>{ country.name }</h2>
-            <p>Native name: { <span>{country.nativeName}</span> }</p>
-            <p>Population: { <span>{country.population}</span> }</p>
-            <p>Region: { <span>{country.region}</span> }</p>
-            <p>Sub region: { <span>{country.subregion}</span> }</p>
-            <p>Capital: { <span>{country.capital}</span> }</p>
+            <p><span>Native name:</span> { <span>{country.nativeName}</span> }</p>
+            <p><span>Population:</span> { <span>{country.population}</span> }</p>
+            <p><span>Region:</span> { <span>{country.region}</span> }</p>
+            <p><span>Sub region:</span> { <span>{country.subregion}</span> }</p>
+            <p><span>Capital:</span> { <span>{country.capital}</span> }</p>
           </div>
           <div>
-            <p>Top level domain: { country.topLevelDomain[0] }</p>
+            <p><span>Top level domain:</span> { country.topLevelDomain[0] }</p>
             <span>Currencies:
               <ul>
                 {
